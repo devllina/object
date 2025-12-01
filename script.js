24c83997274a8092d0760458ca7269acb7e24366
Adelina <adelinatokonova232@gmail.com>
2025-12-01 21:11:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// Object/обьект - бир нерсеге тийиштуу маалыматты сактоочу коробка.
+// let student = {
+//     name: 'Adelina',
+//     age: 17,
+//     growth: 1.70,
+//     profession: 'student',
+//     email: 'adelinatokonova232',
+//     hobby:'reading books',
+// }
+// console.log(student)
+
+// student.age=18 //<=-маалыматты озгортот
+
+// console.log(student)
+
+
+
+// let userName = prompt('атынызды жазыныз')
+// let userPassword = prompt('пароль жазыныз')
+//       const user ={}
+
+// user.Name= 'adelina'
+// user.Password=132536
+// console.log(user)
+
+
+
+// let myphone ={
+//     model:'Iphone 13',
+//     storage: '512gb',
+//     battery: '100%',
+    
+    
+
+
+// }
+// console.log(myphone)
+
+
+let balalyk = {
+    name:'Adelina',
+    age:7,
+    class: '1-класс',
+    hobby: 'сүрөт тартуу',
+
+
+}
+console.log( balalyk)
+
+  balalyk.age=17
+  balalyk.class='student'
+  balalyk.hobby=['reading books','watching Turkish series',]
+  balalyk.favoriteSub='Math'
+
+  
+  console.log( balalyk)
